c980e47920e231301b7a307401251381964951e3
georgiggeorgiev <georgiggeorgiev@users.noreply.github.com>
2020-08-19 18:08:54 +0300

    Update WarningController.java
    TODO

--- client/mobile/android/app/src/main/java/com/joro/driveguard/WarningController.java
+++ client/mobile/android/app/src/main/java/com/joro/driveguard/WarningController.java
@@ -138,6 +138,7 @@ public class WarningController
 
     private void requestLocationUpdate()
     {
+        // TODO Properly request permission
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
         {
             Log.d(TAG, "Location permission error");
